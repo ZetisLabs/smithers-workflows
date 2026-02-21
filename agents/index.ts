c1ffe68de48d4ef7d383463f91e7d974171ae64b
lucienfer <lucien.fernandez02@gmail.com>
2026-02-21 00:26:54 +0000

    update

--- agents/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all agents for easy import
-export { FrontendAgent } from './frontend'
-export { BackendAgent } from './backend'
-export { TestingAgent } from './testing'
-export { ReviewAgent } from './review'
-export { SecurityAgent } from './security'
